2b6f2fe6db7f7d974b90e26443cc0504b5777aa2
mustafa jawadwala <mustafajw07@gmail.com>
2022-04-15 13:16:39 +0530

    DB

--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -3,9 +3,6 @@ const Menu = require('../../models/menu');
 function homeController() {
     return {
         async index(req,res) {
-            // Menu.find().then(function(pizzas){
-            // res.render('home',{pizzas:pizzas})
-            // })
             const pizzas = await Menu.find()
             return res.render('home',{pizzas:pizzas})
         }
