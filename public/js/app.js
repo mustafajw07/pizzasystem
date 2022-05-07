66cd852e6e8ee26541e0c25041b3afc5750e26ff
mustafa jawadwala <mustafajw07@gmail.com>
2022-05-07 17:00:01 +0530

    last

--- public/js/app.js
+++ public/js/app.js
@@ -49,9 +49,10 @@ function initAdmin() {
 
     function generateMarkup(orders) {
         return orders.map(order => {
+            const time = new Date(order.createdAt).toLocaleString();
             return `
                 <tr>
-                <td class="border px-4 py-2 text-green-900">
+                <td class="border px-4 py-2 text-green">
                     <p>${ order._id }</p>
                     <div>${ renderItems(order.items) }</div>
                 </td>
@@ -59,12 +60,10 @@ function initAdmin() {
                 <td class="border px-4 py-2">${ order.address }</td>
                 <td class="border px-4 py-2">${ order.phone }</td>
                 <td class="border px-4 py-2">
-                    <div class="inline-block relative w-64">
+                    <div class="inline-block relative w-50">
                         <form action="/admin/order/status" method="POST">
                             <input type="hidden" name="orderId" value="${ order._id }">
                             <select name="status" onchange="this.form.submit()"
-                                class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
-                                <option value="order_placed"
                                     ${ order.status === 'order_placed' ? 'selected' : '' }>
                                     Placed</option>
                                 <option value="confirmed" ${ order.status === 'confirmed' ? 'selected' : '' }>
@@ -79,18 +78,10 @@ function initAdmin() {
                                 </option>
                             </select>
                         </form>
-                        <div
-                            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20">
-                                <path
-                                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                            </svg>
-                        </div>
                     </div>
                 </td>
                 <td class="border px-2 py-1">
-                    ${order.createdAt}
+                    ${time}
                 </td>
             </tr>
         `
